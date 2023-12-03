37cb3df3583ad58162fda4a64bc3caa7e16e520e
Ameer <ameerapex496@gmail.com>
2023-12-03 03:41:51 +0100

    added function for drag and drop

--- src/script.js
+++ src/script.js
@@ -1,1 +1,36 @@
-console.log("hallo world");
+var fill = document.querySelector(".fill");
+var empties = document.querySelectorAll(".empty");
+if (fill) {
+    fill.addEventListener("dragstart", dragStart);
+    fill.addEventListener("dragend", dragEnd);
+}
+for (var _i = 0, empties_1 = empties; _i < empties_1.length; _i++) {
+    var empty = empties_1[_i];
+    empty.addEventListener("dragover", dragOver);
+    empty.addEventListener("dragenter", dragEnter);
+    empty.addEventListener("dragleave", dragLeave);
+    empty.addEventListener("drop", dragDrop);
+}
+function dragStart() {
+    var _this = this;
+    this.className += " hold";
+    setTimeout(function () { return (_this.className = "invisible"); }, 0);
+}
+function dragEnd() {
+    this.className = "fill";
+}
+function dragOver(e) {
+    e.preventDefault();
+}
+function dragEnter() {
+    this.className += " hovered";
+}
+function dragLeave() {
+    this.className = "empty";
+}
+function dragDrop() {
+    this.className = "empty";
+    if (fill) {
+        this.append(fill);
+    }
+}
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,42 @@
-console.log("hallo world");
+const fill: HTMLElement | null = document.querySelector(".fill");
+const empties: NodeListOf<HTMLElement> = document.querySelectorAll(".empty");
+
+if (fill) {
+  fill.addEventListener("dragstart", dragStart);
+  fill.addEventListener("dragend", dragEnd);
+}
+
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+function dragStart(this: HTMLElement): void {
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisible"), 0);
+}
+
+function dragEnd(this: HTMLElement): void {
+  this.className = "fill";
+}
+
+function dragOver(e: DragEvent): void {
+  e.preventDefault();
+}
+
+function dragEnter(this: HTMLElement): void {
+  this.className += " hovered";
+}
+
+function dragLeave(this: HTMLElement): void {
+  this.className = "empty";
+}
+
+function dragDrop(this: HTMLElement): void {
+  this.className = "empty";
+  if (fill) {
+    this.append(fill);
+  }
+}
